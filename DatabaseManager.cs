2f62f1e3b7d9e9165e02db34537bde2e70e0921d
juankisuelamartin <144780658+juankisuelamartin@users.noreply.github.com>
2023-11-16 14:09:14 +0100

    Inicio sesion y registro

---
+++ DatabaseManager.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+
+public class DatabaseManager
+{
+    private MySqlConnection connection;
+    private string server;
+    private string database;
+    private string uid;
+    private string password;
+
+    public DatabaseManager()
+    {
+        // Conexión BBDD
+        server = "34.175.85.243";
+        database = "db_IPO";
+        uid = "dbIPO";
+        password = "dbIPO";
+        string connectionString;
+        connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+        database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+        connection = new MySqlConnection(connectionString);
+    }
+
+    public MySqlConnection Connection
+    {
+        get { return connection; }
+    }
+
+}
