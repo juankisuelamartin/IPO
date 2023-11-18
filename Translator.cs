2dc006eff610da212d785fddf70b0f14f562ea6b
juankisuelamartin <144780658+juankisuelamartin@users.noreply.github.com>
2023-11-18 14:20:12 +0100

    TRANSLATE BUTTON INICIAR SESION

---
+++ Translator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public static class Translator
+    {
+        private static ResourceDictionary dict;
+
+        public static void Initialize()
+        {
+            dict = new ResourceDictionary();
+            SetLanguage(Thread.CurrentThread.CurrentCulture.ToString());
+        }
+
+        public static void SetLanguage(string culture)
+        {
+            dict.Source = new Uri($"StringResources.{culture}.xaml", UriKind.Relative);
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
+
+        public static void SwitchLanguage(string culture)
+        {
+            Application.Current.Resources.MergedDictionaries.Remove(dict);
+            SetLanguage(culture);
+        }
+
+        public static void SaveSelectedLanguage(string culture)
+        {
+            Properties.Settings.Default.SelectedLanguage = culture;
+            Properties.Settings.Default.Save();
+        }
+
+        public static string GetSelectedLanguage()
+        {
+            return Properties.Settings.Default.SelectedLanguage;
+        }
+    }
+}
